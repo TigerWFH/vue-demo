f1616a4b9c4bf43a4f89532fb822232d5ad04e2b
monkey <monkey>
2021-04-25 18:12:27 +0800

    增加prettier配置

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <component v-for="type in types" :key="type" :is="type" />
+    <component v-for="type in types"
+      :key="type"
+      :is="type" />
   </div>
 </template>
 <script lang="ts">
-import Name from "../components/Name.vue";
-import Age from "../components/Age.vue";
+import Name from "../components/Name.vue"
+import Age from "../components/Age.vue"
 export default {
   data: function () {
     return {
-      types: ["Name", "Age"],
+      types: ["Name", "Age"]
     };
   },
   components: {
     Name,
-    Age,
+    Age
   },
 };
 </script>
